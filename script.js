380e123f53cc7274516e426774daccd29f2b3a13
Hidekashi <hideaki.lawliet@proton.me>
2024-04-01 00:10:35 +0200

    ADD all project
    'cause WHY THE HELL I CAN'T CHANGE MY REPO FROM PRIVATE TO PUBLIC.
    WHY I NEED TO CREATE A NEW ONE WHAT THE FREAK I MISS

---
+++ script.js
@@ -0,0 +1,56 @@
+var W_city  = document.querySelector('.W_city')
+var T_city = document.querySelector('.T_city')
+var humidity = document.querySelector('.humidity')
+var wind = document.querySelector('.wind')
+var description = document.querySelector('.description')
+var refresh = document.querySelector('.refresh')
+
+// W_city += 'Bordeaux';
+
+var name = 'Talence';
+
+refresh.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+name+'&appid=f5436b6a8bb258c06f5922c0752e2215&units=metric')
+    .then(response => response.json())
+    .then(data => {
+        console.log(W_city)
+        var W_cityValue = data['name'];
+        var T_cityValue = data['main']['temp'];
+        var windValue = data['wind']['speed'];
+        var humidityValue = data['main']['humidity'];
+        var descriptionValue = data['weather']['0']['description'];
+        
+        W_city.innerHTML = W_cityValue;
+        T_city.innerHTML = T_cityValue;
+        humidity.innerHTML = humidityValue;
+        wind.innerHTML = windValue;
+        description.innerHTML = descriptionValue;
+        
+        T_city.innerHTML += ' °C';
+        wind.innerHTML += ' Km/h (wind speed)';
+        humidity.innerHTML += ' % (humidity)';
+    })
+})
+
+window.addEventListener("load", (event) => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+name+'&appid=f5436b6a8bb258c06f5922c0752e2215&units=metric')
+    .then(response => response.json())
+    .then(data => {
+        console.log(W_city)
+        var W_cityValue = data['name'];
+        var T_cityValue = data['main']['temp'];
+        var windValue = data['wind']['speed'];
+        var humidityValue = data['main']['humidity'];
+        var descriptionValue = data['weather']['0']['description'];
+        
+        W_city.innerHTML = W_cityValue;
+        T_city.innerHTML = T_cityValue;
+        humidity.innerHTML = humidityValue;
+        wind.innerHTML = windValue;
+        description.innerHTML = descriptionValue;
+        
+        T_city.innerHTML += ' °C';
+        wind.innerHTML += ' Km/h (wind speed)';
+        humidity.innerHTML += ' % (humidity)';
+    })
+  });
