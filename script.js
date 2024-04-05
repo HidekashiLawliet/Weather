5cf3309dd1606eee8a0fc4838f2c8e6f611b2c62
Hidekashi <hideaki.lawliet@proton.me>
2024-04-05 14:47:13 +0200

    HAAA

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 
-var W_city  = document.querySelector('.W_city')
-var T_city = document.querySelector('.T_city')
-var humidity = document.querySelector('.humidity')
-var wind = document.querySelector('.wind')
-var description = document.querySelector('.description')
-var refresh = document.querySelector('.refresh')
+let W_city  = document.querySelector('.W_city')
+let T_city = document.querySelector('.T_city')
+let humidity = document.querySelector('.humidity')
+let wind = document.querySelector('.wind')
+let description = document.querySelector('.description')
+let refresh = document.querySelector('.refresh')
 
 // To change the city you want, you can modify the variable below  //
-var city = 'Bordeaux';
+let city = 'Talence';
 
 window.addEventListener("load", (event) => {
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=f5436b6a8bb258c06f5922c0752e2215&units=metric')
     .then(response => response.json())
     .then(data => {
         console.log(W_city)
-        var W_cityValue = data['name'];
-        var T_cityValue = data['main']['temp'];
-        var windValue = data['wind']['speed'];
-        var humidityValue = data['main']['humidity'];
-        var descriptionValue = data['weather']['0']['description'];
+        let W_cityValue = data['name'];
+        let T_cityValue = data['main']['temp'];
+        let windValue = data['wind']['speed'];
+        let humidityValue = data['main']['humidity'];
+        let descriptionValue = data['weather']['0']['description'];
         
         W_city.innerHTML = W_cityValue;
         T_city.innerHTML = T_cityValue;
@@ -30,4 +30,7 @@ window.addEventListener("load", (event) => {
         wind.innerHTML += ' Km/h (wind speed)';
         humidity.innerHTML += ' % (humidity)';
     })
-  });
+});
+
+
+
