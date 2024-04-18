72d6ae7037f464c1bf313817f73e69d78a23f45f
Hidekashi <hideaki.lawliet@proton.me>
2024-04-18 19:22:10 +0200

    MOD {modify timer depending of the city timezone}

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ let T_city = document.querySelector('.T_city')
 let humidity = document.querySelector('.humidity')
 let wind = document.querySelector('.wind')
 let description = document.querySelector('.description')
-let refresh = document.querySelector('.refresh')
+// let refresh = document.querySelector('.refresh')
 let cels = document.querySelector('.cels')
 let fahr = document.querySelector('.fahr')
-let temp = 0;
 let clock = document.querySelector('.clock')
 let city = "Talence"
 
+
+// * change website title when page is hidden
 window.onload = function() {  
     var pageTitle = document.title;
     var attentionMessage = 'You are missing the weather!';
@@ -19,8 +20,10 @@ window.onload = function() {
         document.title = document.hidden?  `${attentionMessage} \u{1F622}` : pageTitle;  
     });
 }
+// * * * * * * * * * * * * * * * *
 
 
+// * change temp to Fahrenheit or Celsius 
 console.log("this page will automatically refresh every 5 minutes")
 setInterval(function() {
     location.reload();
@@ -38,6 +41,7 @@ cels.addEventListener('change', function() {
         location.reload();
     }
 });
+// * * * * * * * * * * * * * * * *
 
 window.addEventListener("load", (event) => {
     fetch("./settings.json")
@@ -48,9 +52,9 @@ window.addEventListener("load", (event) => {
                 console.error("Please set a city in the settings.json file");
             }
             city = data.city;
-            console.log(`json city: ${city}`);
             return city;
     })
+
     .then( city =>
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f5436b6a8bb258c06f5922c0752e2215&units=metric`)
         .then(response => response.json())
@@ -60,7 +64,11 @@ window.addEventListener("load", (event) => {
             let humidityValue = data['main']['humidity'];
             let descriptionValue = data['weather']['0']['description'];
             let timezone = data['timezone']
-            
+
+            setInterval(function() {
+                upDate(timezone);
+            }, 1000)
+
             if (fahr.checked) {
                 T_cityValue = T_cityValue * 1.8 + 32;
                 T_cityValue = Math.round(T_cityValue * 100) / 100;
@@ -69,7 +77,6 @@ window.addEventListener("load", (event) => {
                 T_cityValue = T_cityValue;
                 T_city.innerHTML = T_cityValue + ' °C';
             }
-            console.log('city: ' + city)
             W_city.innerHTML = city;
             humidity.innerHTML = humidityValue;
             wind.innerHTML = windValue;
@@ -81,27 +88,22 @@ window.addEventListener("load", (event) => {
     )
 });
 
-
-setInterval(function() {
-    upDate();
-}, 1000);
-
-function upDate() {
+function upDate(timezone) {
     let date = new Date();
-    let hours = date.getHours().toString().padStart(2, '0');
     let minutes = date.getMinutes().toString().padStart(2, '0');;
     let seconds = date.getSeconds().toString().padStart(2, '0');;
+    let cityHours = timezone;
+    let hours;
 
+    cityHours /= 60;
+    cityHours /= 60;
+    cityHours -= 2;
+
+    hours = cityHours + date.getHours(); 
+
+    if (hours > 24) {
+        hours -= 24;
+    }
+    hours = hours.toString().padStart(2, '0');
     clock.innerHTML = hours + ':' + minutes + ':' + seconds;
 }
-
-console.log('================================');
-
-// ! Khatanga hour = Paris hours + (city.timezone(to converte in hours) - 2) 
-// ! Tokyo hour = Paris hours + (city.timezone(to converte in hours) - 2)
-// ! for est country
-// ! for Manitoba = Paris hours + (- city.timezone(to converte in hours) - 2)
-
-
-// ! for est country X = x/60 x = x/60
-// ! for west country x = -x/60 x = x/60
