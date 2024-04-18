b563ff5e195e35a1ef13667fbb90749599c88da3
Hidekashi <hideaki.lawliet@proton.me>
2024-04-18 18:41:55 +0200

    FIX {bug- don't get all data from the API}

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ let temp = 0;
 let clock = document.querySelector('.clock')
 let city = "Talence"
 
-
-window.onload = function() {
+window.onload = function() {  
     var pageTitle = document.title;
     var attentionMessage = 'You are missing the weather!';
     
@@ -40,44 +39,49 @@ cels.addEventListener('change', function() {
     }
 });
 
-
-fetch("./settings.json")
-    .then(response => response.json())
-    .then(data => {
-        if (data.city === "") {
-            data.city = "Talence";
-            console.error("Please set a city in the settings.json file");
-        }
-        city = data.city;
-});
-
 window.addEventListener("load", (event) => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=f5436b6a8bb258c06f5922c0752e2215&units=metric')
-    .then(response => response.json())
-    .then(data => {
-        let T_cityValue = data['main']['temp'];
-        let windValue = data['wind']['speed'];
-        let humidityValue = data['main']['humidity'];
-        let descriptionValue = data['weather']['0']['description'];
-    
-    
-    if (fahr.checked) {
-        T_cityValue = T_cityValue * 1.8 + 32;
-        T_cityValue = Math.round(T_cityValue * 100) / 100;
-        T_city.innerHTML = T_cityValue + ' °F';
-    } else {
-        T_cityValue = T_cityValue;
-        T_city.innerHTML = T_cityValue + ' °C';
-    }
-    W_city.innerHTML = city;
-    humidity.innerHTML = humidityValue;
-    wind.innerHTML = windValue;
-    description.innerHTML = descriptionValue;
-    
-    wind.innerHTML += ' Km/h (wind speed)';
-    humidity.innerHTML += ' % (humidity)';
-})
+    fetch("./settings.json")
+        .then(response => response.json())
+        .then(data => {
+            if (data.city === "") {
+                data.city = "Talence";
+                console.error("Please set a city in the settings.json file");
+            }
+            city = data.city;
+            console.log(`json city: ${city}`);
+            return city;
+    })
+    .then( city =>
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f5436b6a8bb258c06f5922c0752e2215&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            let T_cityValue = data['main']['temp'];
+            let windValue = data['wind']['speed'];
+            let humidityValue = data['main']['humidity'];
+            let descriptionValue = data['weather']['0']['description'];
+            let timezone = data['timezone']
+            
+            if (fahr.checked) {
+                T_cityValue = T_cityValue * 1.8 + 32;
+                T_cityValue = Math.round(T_cityValue * 100) / 100;
+                T_city.innerHTML = T_cityValue + ' °F';
+            } else {
+                T_cityValue = T_cityValue;
+                T_city.innerHTML = T_cityValue + ' °C';
+            }
+            console.log('city: ' + city)
+            W_city.innerHTML = city;
+            humidity.innerHTML = humidityValue;
+            wind.innerHTML = windValue;
+            description.innerHTML = descriptionValue;
+            
+            wind.innerHTML += ' Km/h (wind speed)';
+            humidity.innerHTML += ' % (humidity)';
+        })
+    )
 });
+
+
 setInterval(function() {
     upDate();
 }, 1000);
@@ -91,3 +95,13 @@ function upDate() {
     clock.innerHTML = hours + ':' + minutes + ':' + seconds;
 }
 
+console.log('================================');
+
+// ! Khatanga hour = Paris hours + (city.timezone(to converte in hours) - 2) 
+// ! Tokyo hour = Paris hours + (city.timezone(to converte in hours) - 2)
+// ! for est country
+// ! for Manitoba = Paris hours + (- city.timezone(to converte in hours) - 2)
+
+
+// ! for est country X = x/60 x = x/60
+// ! for west country x = -x/60 x = x/60
